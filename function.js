0d91ec8876be396066832571b4cf3e14cfe4a97c
JenyColegado <colegadojeny5@gmail.com>
2025-02-24 12:02:45 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,24 @@
+function changeColor(){
+
+const colorText = document.getElementById("jini");
+colorText.innerText = "h1";
+colorText.style.color = "red";
+colorText.style.backgroundColor = "black";
+colorText.style.padding = "90px";
+document.body.style.backgroundColor = "pink";
+}
+{
+
+
+    
+    
+    function inputText(){
+
+        const textBox = document.getElementById("myInput").value;
+        alert(textBox);
+
+    }
+}
+
+
+
