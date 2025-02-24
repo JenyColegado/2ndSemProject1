a141296bf5bd1ddfa8d481507e2eedbe881ea8ea
JenyColegado <colegadojeny5@gmail.com>
2025-02-24 11:55:05 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const arr = [1, 10, 6, 4, 9 ];
+ console.log(arr.includes(6));
+
+
+ const checkValue = arr.includes(10);
+ 
+
+
+ const names = ["Zake", "Jeevie", "Jyles", 'Ape', "Ken", "Mark"];
+ const checkName = names.includes("Jyles");
+ console.log(checkName);
+
+
+ const inOrder = names.sort();
+ console.log(inOrder);
+
+ const numberOrder = arr.sort((a,b) => (a-b)).reverse()
+ console.log(numberOrder);  
+ 
+ 
+
+ const jia = [ 1, 10, 3, 8, 5 ];
+
+ console.log(jia.includes(10, 3));
+ 
